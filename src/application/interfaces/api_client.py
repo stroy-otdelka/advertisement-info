038f65f74ed2a9a6905e06bace0db981abcc3202
seregaziguli@gmail.com <seregaziguli@gmail.com>
2025-01-22 15:34:23 +0300

    delete useless methods

--- src/application/interfaces/api_client.py
+++ src/application/interfaces/api_client.py
@@ -1,8 +1,6 @@
 from abc import abstractmethod, ABC
 from typing import Any
 
-
-
 class AbstractApiClient(ABC):
     @abstractmethod
     async def get(self, url: str, **kwargs: Any) -> dict | None:
@@ -36,10 +34,6 @@ class AbstractOzonApiClient(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    async def get_sale_ozon(self, seller: str) -> dict:
-        raise NotImplementedError
-
-    @abstractmethod
     async def _get_all_adv(self, seller: str, **kwargs: Any) -> dict | None:
         """
         :param url: Адрес Запроса
@@ -61,16 +55,9 @@ class AbstractOzonApiClient(ABC):
         :param url: Адрес Запроса
         :param kwargs: Параметры Запроса
         """
-        
-    async def get_sku_to_quantity(self, seller: str) -> dict[int, int]:
-        raise NotImplementedError
 
     @abstractmethod
     async def get_ozon_product(self, vendor_code_wb: str, seller: str) -> dict:
-        raise NotImplementedError
-
-    @abstractmethod
-    async def get_sales_ozon(self, seller: str) -> list:
         raise NotImplementedError
 
     @abstractmethod
